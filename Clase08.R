4dfc810820318afc89e99afe8864c606279bd3c9
duhi23 <diego.21.phs@hotmail.com>
2015-12-09 12:51:59 -0500

    UPdate Script 08

--- Clase08.R
+++ Clase08.R
@@ -7,6 +7,7 @@ library(dplyr)
 datos <- read_sav("base_modelos.sav")
 glimpse(datos)
 
+# Función de cortes - 5 dias
 cortes_5 <- function(vector){
       cortes <- c(-1,0,5,10,15,30,60,90,2000)
       etiquetas <- c("Sin vencido", "1-5 dias", "6-10 dias",
@@ -29,6 +30,7 @@ datos[["DIAS_VEN_N1"]][datos[["SALDO_N1"]]<42] <- 0
 saldos <- paste("SALDO_N",c(1:12), sep="")
 dias <- paste("DIAS_VEN_N",c(1:12), sep="")
 
+# Enceramos los días en base a los saldos
 for(i in 1:12){
       datos[[dias[i]]][datos[[saldos[i]]]<42] <- 0
 }
@@ -36,23 +38,8 @@ for(i in 1:12){
 
 table(cortes_5(datos[["DIAS_VEN_N1"]]), cortes_5(datos[["DIAS_VEN_N2"]]))
 
+# Matriz de transición
 for(i in 1:11){
       cat("Tabla ", dias[i], " vs ", dias[i+1])
       print(table(cortes_5(datos[[dias[i]]]), cortes_5(datos[[dias[i+1]]])))
 }
-
-
-
-      
-      
-      
-      
-      
-      
-      
-      
-      
-
-
-
-
