94ccb99e6b5cf298f9f1676c9a7e87b8ef471536
duhi23 <diego.21.phs@hotmail.com>
2015-12-18 11:57:28 -0500

    Update

--- Clase10.R
+++ Clase10.R
@@ -2,19 +2,23 @@
 #####     KS Function      #####
 ################################
 
+datos$GB_60 <- as.integer(datos$GB_60)
+GB <- as.integer(datos[["GB_60"]])
+
+which(unlist(lapply(datos,class))=="labelled")
 
 test_ks <- function (x){
       no_missg <- length(x[!is.na(x)])/length(x)
-      if(is.numeric(x) && no_missg > 0.50){
+      if(is.numeric(x) && no_missg > 0.80){
             vars <- data.frame(GB,x)
-            var_m <- vars[vars[,1]==1,2] 
+            var_m <- vars[vars[,1]==1,2]
             var_b <- vars[vars[,1]==0,2]
             ks <- ks.test(var_b,var_m, alternative = c("two.sided"))
             ks <- as.numeric(ks$statistic)        
       }else{
             ks <- as.numeric(-999)
       }
-      return(ks) 
+      return(ks)
 }
 
 
@@ -25,9 +29,11 @@ KS <- c(0)
 for (i in seq(1:ncol(vars)))
 {
       KS[i] <- test_ks(vars[,i])
-} 
+}
+
 
 ## Orden mejores variables segun KS
+nom <- as.character(colnames(datos))
 KS <- data.frame(nom,KS)
 KS <- KS[order(KS[,2],decreasing = TRUE),]
 
