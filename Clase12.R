17bee8862b46e530addb8ed74145621a8cab24e6
duhi23 <diego.21.phs@hotmail.com>
2016-01-15 12:22:06 -0500

    Segmentacion

---
+++ Clase12.R
@@ -0,0 +1,20 @@
+##########################################
+#######        Segmentación        #######
+##########################################
+
+library(haven)
+library(dplyr)
+datos <- read_sav("trabajo_modelos.sav")
+glimpse(datos)
+
+subdata <- datos %>% select(DIGITO, NIVEL_RIESGO, INGRESOESTIMADO, TARJETAS_CREDITO)
+
+
+# K medias
+CLUSTER <- kmeans(subdata,3,iter.max=20)
+str(CLUSTER)
+subdata$CLUSTER <- CLUSTER$cluster
+
+round((CLUSTER$size/sum(CLUSTER$size))*100,digits=2)
+
+plot(subdata[c("NIVEL_RIESGO","INGRESOESTIMADO")], col=subdata$CLUSTER)
